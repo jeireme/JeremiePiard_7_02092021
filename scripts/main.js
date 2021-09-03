28cfac5d849956de728d45fc2c3b011c7ae8f6a8
Jérémie PIARD <jeremie.piard@yahoo.fr>
2021-09-03 02:57:49 +0200

    création de la page d'accueil

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 import DisplayManager from './displayManager.js';
 
-// const url = "https://raw.githubusercontent.com/jeireme/...";
-const url = "../data/recipes.json";
+const url = "https://raw.githubusercontent.com/jeireme/JeremiePiard_7_02092021/master/data/recipes.json";
+// const url = "../data/recipes.json";
 
 fetch(url).then(response => {
         if (response.ok) return response.json();
