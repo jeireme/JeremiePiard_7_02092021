bf8136b8fec01452ae4d111bbcaa2c409ec9abea
Jérémie PIARD <jeremie.piard@yahoo.fr>
2021-09-03 11:35:34 +0200

    premiers tests de la search bar

--- scripts/displayManager.js
+++ scripts/displayManager.js
@@ -1,6 +1,8 @@
 import Recipe from './recipe.js';
 
 let recipies = [];
+let searchBar = document.getElementById("searchBar");
+let searchReg = new RegExp(/[a-zA-Z]{3,}/);
 
 export default class DisplayManager {
 
@@ -14,5 +16,22 @@ export default class DisplayManager {
             recipe.display();
             recipies.push(recipe);
         }
+
+        searchBar.addEventListener("input", onSearch);
     }
+}
+
+function onSearch(event) {
+    for (let recipe of recipies) recipe.appendChild();
+
+    if (searchReg.test(event.target.value)) {
+        for (let recipe of recipies) {
+            if (!recipe.name.toLowerCase().includes(event.target.value.toLowerCase())) {
+                // console.log("recipe.name = " + recipe.name);
+                recipe.removeChild();
+            }
+        }
+    }
+
+
 }
