a5ac93995c0116dc68abe85e2dcc18eeb99ae551
Jérémie PIARD <jeremie.piard@yahoo.fr>
2021-11-09 02:05:27 +0100

    ajout d'une classe Main

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,27 @@
 import DisplayManager from './displayManager.js';
 
-const url = "https://raw.githubusercontent.com/jeireme/JeremiePiard_7_02092021/master/data/recipes.json";
+class Main {
 
-fetch(url).then(response => {
-        if (response.ok) return response.json();
-        else throw new Error('Something went wrong');
-    })
-    .then(datas => {
-        let display = new DisplayManager(datas);
-        display.homepage();
-    })
-    .catch((error) => {
-        console.log(error)
-    });
+    url = "https://raw.githubusercontent.com/jeireme/JeremiePiard_7_02092021/master/data/recipes.json";
+
+    constructor() {
+        this._fetchData();
+    }
+
+    async _fetchData() {
+        try {
+            const data = await fetch(this.url);
+            if (data) {
+                const json = await data.json();
+                let display = new DisplayManager(json);
+                display.homepage();
+            }
+        } catch (err) {
+            throw new Error(err);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Main();
+});
